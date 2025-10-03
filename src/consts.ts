309233328b89459649ab7fe3cb629bd0290297cb
Thomas Robbe <thomasrobbedev@gmail.com>
2025-10-03 16:07:39 +0200

    add some CSS

--- src/consts.ts
+++ src/consts.ts
@@ -2,4 +2,5 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'Thomas Robbe Dev';
+export const H1_WORDING = 'Dev';
 export const SITE_DESCRIPTION = 'Welcome to my website!';
